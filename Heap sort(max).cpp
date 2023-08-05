21f8143dfa6d4a79b3d3d58e09fdc8dbcb73b86c
siMobin <shakibulislammobin@gmail.com>
2023-08-05 19:28:49 +0600

    upload

---
+++ Heap sort(max).cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void heapefy(vector<int>&array,int i,int n)
+{
+
+    int largest =i,left,right;
+    left=2*i+1;
+    right=2*i+2;
+    if(left<n && array[left]>array[largest])
+    {
+        largest=left;
+
+    }
+
+    if(right<n && array[right]>array[largest])
+    {
+        largest=right;
+
+    }
+
+    if(largest != i)
+    {
+        swap(array[i],array[largest]);
+        heapefy(array,largest,n);
+    }
+}
+
+void heap(vector<int>& array)
+{
+    int i, n=array.size();
+    for(i=n/2-1; i>=0; i--)
+    {
+        heapefy(array,i,n);
+    }
+    for(i=n-1; i>0; i--)
+    {
+        swap(array[0],array[i]);
+        heapefy(array,0,i);
+    }
+}
+
+
+int main()
+{
+    vector<int> array= {2,9,0,3,5};
+    heap(array);
+    for(int a : array)
+    {
+        cout<<a<<" ";
+    }
+    return 0;
+}
+
+
---
+++ shell sort.cpp
@@ -0,0 +1,67 @@
+/*
+A : array of items
+   while interval < A.length /3 do:
+      interval = interval * 3 + 1
+   end while
+
+   while interval > 0 do:
+      for outer = interval; outer < A.length; outer ++ do:
+      valueToInsert = A[outer]
+      inner = outer;
+         while inner > interval -1 && A[inner - interval] >= valueToInsert do:
+            A[inner] = A[inner - interval]
+            inner = inner - interval
+         end while
+      A[inner] = valueToInsert
+
+      end for
+   interval = (interval -1) /3;
+
+   end while
+
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,interval,lenght,outer,valuetoinsert,inner;
+    cout<<"enter size of array :";
+    cin>>n;
+    int a[n];
+    for(i=0; i<n; i++)
+        cin>>a[i];
+    while(interval < n/3)
+    {
+        interval = (interval *3)+1;
+    }
+    while(interval >0)
+    {
+        for(outer=interval; outer <n; outer ++)
+        {
+            valuetoinsert=a[outer];
+            inner=outer;
+            while(inner > interval-1 && a[inner-interval] >= valuetoinsert)
+            {
+                a[inner]=a[inner-interval];
+                inner=inner-interval;
+            }
+            a[inner]=valuetoinsert;
+
+            for(i=0; i<n; i++)
+            {
+                cout<<a[i]<<" ";
+            }
+            cout<<endl;
+        }
+        interval=(interval-1)/3;
+    }
+    for(i=0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
+
+//shell sort
